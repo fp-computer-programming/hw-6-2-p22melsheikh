e7ef0aa4a619b70ad544a9f9daada8ef9470c8cb
x <xxx@tsststs.com>
2021-11-16 20:48:12 -0500

    hw i submit late becuase i didnt have laptop during college visit

--- hw6-2.py
+++ hw6-2.py
@@ -10,3 +10,22 @@ print(len(my_lst))
 print(my_lst[3])
 
 # Question 4
+print(5 in [1, 2, 3])
+
+# Question 5
+print(10 in [8, 9, 10])
+
+# Question 6
+classmate_names = ['mateo', 'micheal', 'jordan', 'gussen']
+
+# Question 7
+print(classmate_names[3])
+
+# Question 8
+print(classmate_names * 3)
+
+# Question 9
+full_list = my_lst + classmate_names
+
+# Question 10
+print(len(full_list))
